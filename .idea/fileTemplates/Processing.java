5614db8325d5e05a30eb3a51de7841dbb73f554c
swefd <swefd>
2021-10-30 11:25:24 +0300

    File Template

---
+++ .idea/fileTemplates/Processing.java
@@ -0,0 +1,40 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+import com.robocode.Features;
+import processing.core.PApplet;
+
+public class ${NAME} extends PApplet{
+
+    Features feature = new Features(this);
+
+    // Метод для налаштувань програми
+    public void settings(){
+        size(500, 500);
+    }
+
+
+    // Метод для одноразового малювання при запуску програми
+    public void setup(){
+        background(255);
+    }
+
+    // Медод для малювання в циклі
+    public void draw(){
+        feature.drawGrid(10, 50); //Малювання сітки
+    }
+
+
+    // Метод який запускає програму
+    // Генерується автоматично
+    public static void main(String[] args) {
+        /*
+        У випадку ручного налаштування:
+        В параметрах потрібно вказати пакет і назву поточного класу який запускається
+        Наприклад:
+        PApplet.main("пакет.НазваКласу");
+        */
+        PApplet.main("${PACKAGE_NAME}.${NAME}");
+    }
+
+}
